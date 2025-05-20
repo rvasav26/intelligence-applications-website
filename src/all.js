050c8b510b115d6f40aeb6f5def41e95146f1633
Rhushil Vasavada <rhushilvasavada@gmail.com>
2025-05-20 17:24:45 -0500

    Changing home.html to index.html

--- src/all.js
+++ src/all.js
@@ -1,13 +1,13 @@
 var goHomeImg = document.createElement("img");
 goHomeImg.id = "goHomeImg"
-goHomeImg.src = "images/GoHome.png";
+goHomeImg.src = "../images/GoHome.png";
 
 
 goHomeImg.title="Go Back to Home Page"
 
 var goHomeA = document.createElement("a");
 
-goHomeA.href = "home.html";
+goHomeA.href = "../index.html";
 
 goHomeA.appendChild(goHomeImg);
 
